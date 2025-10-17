3a2d49ddb2fb35d6f535f635e8c8fec2039fd778
Kevingarzon94 <kevingarzon4@gmail.com>
2025-10-17 17:10:24 -0500

    fix: add cors

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ dotenv.config()
 
 const app = express();
 app.set('trust proxy', 1);
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+}))
 const PORT = Number(process.env.PORT) || 8080;
 
 app.use(cors())
