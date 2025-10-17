e67cca5377769c26c95417ec2b1e04cb313ecdd4
Kevingarzon94 <kevingarzon4@gmail.com>
2025-10-17 17:33:00 -0500

    Fix: delete duplicated cors

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,16 @@ import userChatRouter from "./routes/userChat.route";
 dotenv.config()
 
 const app = express();
+
 app.set('trust proxy', 1);
+
 app.use(cors({
     origin: ['http://localhost:5173'],
     credentials: true,
 }))
+
 const PORT = Number(process.env.PORT) || 8080;
 
-app.use(cors())
 app.use(express.json())
 
 app.use(userChatRouter)
