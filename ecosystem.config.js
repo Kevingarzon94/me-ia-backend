ac8f12e5a0c41594a27321160b0ccd247211c43c
Kevin Garzon <kevingarzon4@gmail.com>
2026-07-28 23:43:46 -0500

    chore:modify ecosystem confing

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     apps : [{
-        name: "mi-backend",
+        name: "me-ia-backend",
         script: "./dist/index.js",
         instances: "max",
         exec_mode: "cluster",
